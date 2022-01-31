e40fa93eb9d9296b48ec536f4986ccd4fb561399
shekhar16 <shekharlaad1609@gmail.com>
2022-01-31 23:47:14 +0530

    fix: corrections for comments and loggers

--- api-rest/src/main/java/org/gluu/oxtrust/api/server/model/SingleSignOnServices.java
+++ api-rest/src/main/java/org/gluu/oxtrust/api/server/model/SingleSignOnServices.java
@@ -7,9 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SingleSignOnServices  implements Serializable{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -7253683158021416550L;
 	private String binding;
 	private String location;
