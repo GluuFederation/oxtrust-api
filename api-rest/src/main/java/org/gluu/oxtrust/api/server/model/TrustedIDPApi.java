274e021cff3aacc656e97e43b1d65f96b73f33d4
shekhar16 <shekharlaad1609@gmail.com>
2022-02-11 21:32:45 +0530

    fix: change in request payload and response

--- api-rest/src/main/java/org/gluu/oxtrust/api/server/model/TrustedIDPApi.java
+++ api-rest/src/main/java/org/gluu/oxtrust/api/server/model/TrustedIDPApi.java
@@ -10,99 +10,36 @@ import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "remoteIdp")
+@XmlType(name = "trustedIdp")
 public class TrustedIDPApi implements Serializable {
 
-	public TrustedIDPApi(String inum, String name, String host, String signingCertificates,
-			List<SingleSignOnServices> supportedSingleSignOnServices,
-			SingleSignOnServices selectedSingleSignOnService) {
-		super();
-		//this.inum = inum;
-		this.name = name;
-		this.host = host;
-		this.signingCertificates = signingCertificates;
-		this.supportedSingleSignOnServices = supportedSingleSignOnServices;
-		this.selectedSingleSignOnService = selectedSingleSignOnService;
-	}
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5914998360095334159L;
-	//private String inum;
-	private String name;
-	private String host;
-	private String signingCertificates;
-	private List<SingleSignOnServices> supportedSingleSignOnServices;
+	private RemoteIdp remoteIdp;
 	private SingleSignOnServices selectedSingleSignOnService;
-
-	public TrustedIDPApi() {
-	}
-
-	/*
-	 * public String getInum() { return inum; }
-	 * 
-	 * public void setInum(String inum) { this.inum = inum; }
-	 */
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public String getSigningCertificates() {
-		return signingCertificates;
-	}
-
-	public void setSigningCertificates(String signingCertificates) {
-		this.signingCertificates = signingCertificates;
-	}
-
-	public List<SingleSignOnServices> getSupportedSingleSignOnServices() {
-		return supportedSingleSignOnServices;
-	}
-
-	public void setSupportedSingleSignOnServices(List<SingleSignOnServices> supportedSingleSignOnServices) {
-		this.supportedSingleSignOnServices = supportedSingleSignOnServices;
-	}
-
+	
+	
+	
 	public SingleSignOnServices getSelectedSingleSignOnService() {
 		return selectedSingleSignOnService;
 	}
-
 	public void setSelectedSingleSignOnService(SingleSignOnServices selectedSingleSignOnService) {
 		this.selectedSingleSignOnService = selectedSingleSignOnService;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public RemoteIdp getRemoteIdp() {
+		return remoteIdp;
 	}
-
+	public void setRemoteIdp(RemoteIdp remoteIdp) {
+		this.remoteIdp = remoteIdp;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((host == null) ? 0 : host.hashCode());
-		//result = prime * result + ((inum == null) ? 0 : inum.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((getRemoteIdp() == null) ? 0 : getRemoteIdp().hashCode());
 		result = prime * result + ((selectedSingleSignOnService == null) ? 0 : selectedSingleSignOnService.hashCode());
-		result = prime * result + ((signingCertificates == null) ? 0 : signingCertificates.hashCode());
-		result = prime * result
-				+ ((supportedSingleSignOnServices == null) ? 0 : supportedSingleSignOnServices.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -112,37 +49,17 @@ public class TrustedIDPApi implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		TrustedIDPApi other = (TrustedIDPApi) obj;
-		if (host == null) {
-			if (other.host != null)
+		if (getRemoteIdp() == null) {
+			if (other.getRemoteIdp() != null)
 				return false;
-		} else if (!host.equals(other.host))
-			return false;
-		/*
-		 * if (inum == null) { if (other.inum != null) return false; } else if
-		 * (!inum.equals(other.inum)) return false;
-		 */
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
+		} else if (!getRemoteIdp().equals(other.getRemoteIdp()))
 			return false;
 		if (selectedSingleSignOnService == null) {
 			if (other.selectedSingleSignOnService != null)
 				return false;
 		} else if (!selectedSingleSignOnService.equals(other.selectedSingleSignOnService))
 			return false;
-		if (signingCertificates == null) {
-			if (other.signingCertificates != null)
-				return false;
-		} else if (!signingCertificates.equals(other.signingCertificates))
-			return false;
-		if (supportedSingleSignOnServices == null) {
-			if (other.supportedSingleSignOnServices != null)
-				return false;
-		} else if (!supportedSingleSignOnServices.equals(other.supportedSingleSignOnServices))
-			return false;
 		return true;
 	}
-
 
 }
